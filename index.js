408150b70392f9a22849ef05b8661b3531c48006
drftgyuhiju7 <js1zeldabee@hotmail.com>
2024-05-09 18:19:24 +0700

    feat task

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const map = new Map();
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  for (const char of t) {
+    if (!map.has(char) || map.get(char) === 0) return false;
+    map.set(char, map.get(char) - 1);
+  }
+  return true;
+}
